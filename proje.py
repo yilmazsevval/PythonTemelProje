31dfee7a6457960e9a6b21a8ad9f46295a2b8d59
sevval yilmaz <yilmaz.sevvall99@gmail.com>
2022-08-24 09:43:53 +0300

    ILKMESAJ

---
+++ proje.py
@@ -0,0 +1,10 @@
+input1= [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+output1=[]
+def flatten(x):
+    for i in x:
+        if  isinstance(i,list):
+            flatten(i)
+        else:
+            output1.append(i)
+    return output1
+print(flatten(input1))
---
+++ proje2.py
@@ -0,0 +1,9 @@
+from re import L
+
+
+l = [[1, 2], [3, 4], [5, 6, 7]]
+l = l[::-1]
+
+for i in range(len(l)):
+    (l[i]) = (l[i]) [::-1]
+print(l)
